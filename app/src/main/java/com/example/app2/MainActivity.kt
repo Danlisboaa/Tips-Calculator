a99eb1e6c15875e8bded04b235b943b8296e7d23
Daniel Lisboa <daniel.lisboa.alves@gmail.com>
2025-01-18 19:33:57 -0300

    mudando o FindViewById para os views Binding

--- app/src/main/java/com/example/app2/MainActivity.kt
+++ app/src/main/java/com/example/app2/MainActivity.kt
@@ -8,21 +8,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.app2.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity: AppCompatActivity (){
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val btnClean = findViewById<Button>(R.id.btn_clean)
-        val btnDone =findViewById<Button>(R.id.btn_done)
-        val edtBill = findViewById<TextInputEditText>(R.id.total_bill)
-        val edtNumPeople = findViewById<TextInputEditText>(R.id.num_people)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.rbOptionOne.setOnCheckedChangeListener {_, isChecked ->
+            println("Option one : $isChecked")
+        }
+        binding.rbOptionTwo.setOnCheckedChangeListener{_, isChecked ->
+            println("Option two : $isChecked")
+        }
+        binding.rbOptionThree.setOnCheckedChangeListener {_,isChecked ->
+            println("Option Three : $isChecked")
+        }
 
+        binding.btnClean.setOnClickListener{
+            println("Daniel 1" + binding.totalBill.text)
+            println("Daniel 1" + binding.numPeople.text)
 
-        btnClean.setOnClickListener{
-
+        }
+        binding.btnDone.setOnClickListener{
+            val total: Float = binding.totalBill.text.toString().toFloat()
+            val nPeople: Float = binding.numPeople.text.toString().toFloat()
         }
     }
 }
